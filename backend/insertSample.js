b5330514465c94037c775621f2b08d387a2086b8
Abin Raj <abinrajmk8@gmail.com>
2025-03-10 21:38:58 +0530

    message

--- backend/insertSample.js
+++ backend/insertSample.js
@@ -7,7 +7,7 @@ const DEVICE_INFO = [
     { id: 1, name: 'Device 1', ip: '192.168.29.1', mac: '00:00:00:00:00:00', status: 'High', netId: '192.168.29' },
     { id: 2, name: 'Device 2', ip: '192.168.1.1', mac: '00:00:00:00:00:00', status: 'Low', netId: '192.168.1' },
     { id: 3, name: 'Device 3', ip: '192.168.29.4', mac: '00:00:00:00:00:00', status: 'High', netId: '192.168.29' },
-    { id: 4, name: 'Device 4', ip: '172.16.0.10', mac: '00:00:00:00:00:00', status: 'Low', netId: '172.16.0' },
+    { id: 4, name: 'Device 5', ip: '172.16.0.10', mac: '00:00:00:00:00:00', status: 'Low', netId: '172.16.0' },
   ];
 
 connectDB();
